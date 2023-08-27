0875514776001195bc771d45958549a0d14627f3
JosephDeFacto <josip.mareljic@gmail.com>
2023-08-27 23:36:36 +0200

    Implemented the ability for users to update their password through the account settings page.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,13 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     //
     public function account()
     {
-        return view('user.account');
+
+        $user = auth()->user();
+
+        return view('user.account', compact('user'));
+    }
+
+    public function create()
+    {
+        return view('user.create');
+    }
+
+    public function store(Request $request)
+    {
+
+        $user = Auth::user();
+
+        $request->validate([
+            'current_password' => 'required',
+            'new_password' => 'required|string|min:6|confirmed',
+        ]);
+
+        if (Hash::check($request->current_password, $user->password)) {
+            $user->update(['password' => Hash::make($request->new_password)]);
+            return redirect()->route('home')->with('success', 'Password changed.');
+        } else {
+            return back()->withErrors(['current_password' => 'Incorrect current password']);
+        }
+
+
+
+        return redirect()->to('/login');
     }
 }
--- resources/views/user/account.blade.php
+++ resources/views/user/account.blade.php
@@ -1,7 +1,5 @@
-<p>
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus luctus fringilla sem. 
-Vivamus et fermentum eros. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aenean at malesuada enim, a dignissim odio. Ut ut blandit massa, at porta neque. 
-Nulla pulvinar purus et gravida auctor. Vestibulum vel metus finibus, scelerisque tortor ut, blandit turpis. Vivamus bibendum interdum semper.
-</p>
-
-<a href="#change-pass">Change password</a>
+@if(isset($user))
+    {{ $user->name }}
+    {{ $user->email }}
+@endif
+<a href="{{ url('account/update-password') }}">Change password</a>
