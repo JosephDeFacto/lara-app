797b8dd3e8f642ec1c501173e6417fc927ba7648
JosephDeFacto <josip.mareljic@gmail.com>
2023-09-01 22:04:42 +0200

    Apply price on quantity and add nice-looking cart-page design.

--- resources/views/cart/viewCart.blade.php
+++ resources/views/cart/viewCart.blade.php
@@ -1,37 +1,51 @@
 @extends('welcome')
 @section('content')
 
-    <h1>Cart Items</h1>
-    @php
-         $currency = '$';
-    @endphp
-    @if ($cart)
-        <ul>
-            @foreach ($cart->cartItems as $cartItem)
-                {{ $cartItem->product->name }}<br>
-                {{ $cartItem->product->description }}<br>
-                {{ $cartItem->product->price }}{{ $currency }}<br>
-                 Quantity: {{ $cartItem->quantity }}<br>
-                <form class="change-quantity-form">
-                    @csrf
-                    <input type="hidden" name="product_id" value="{{ $cartItem->product->id }}">
-                    <button type="button" class="change-quantity-btn" data-action="decrement">-</button>
-                    <span class="quantity">{{ $cartItem->quantity }}</span>
-                    <button type="button" class="change-quantity-btn" data-action="increment">+</button>
+    <div class="container mx-auto px-4 py-8">
 
-                    {{--<input type="number" name="quantity" value="{{ $cartItem->quantity }}" min="1" max="10">--}}
-                    {{--<input type="submit" value="submit">--}}
-                </form>
-                <a href="{{ route('cart.destroy', ['cartItemId' => $cartItem->id]) }}">Remove</a>
-                <img src="{{ asset('storage/images/'.$cartItem->product->image) }}" style="height: 250px;width:250px;">
-            @endforeach
-            <a href="{{ url('checkout') }}">Proceed to checkout</a>
-        </ul>
+        <div class="mt-8">
+            @if(isset($cart->cartItems) && count($cart->cartItems) > 0)
+                <div class="flex flex-col md:flex-row md:justify-between md:items-center">
+                    <h1 class="text-2xl font-bold my-4">Shopping Cart</h1>
+                    <button class="button-proceed bg-indigo-500 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded">
+                        <a href="{{ url('checkout') }}">Proceed to checkout</a>
+                    </button>
+                </div>
+                @foreach($cart->cartItems as $cartItem)
+                    <div class="flex flex-col md:flex-row border-b border-gray-400 py-4">
+                        <div class="flex-shrink-0">
+                            <img src="{{ asset('storage/images/'.$cartItem->product->image) }}" alt="Product image" class="w-32 h-32 object-cover">
+                        </div>
+                        <div class="mt-4 md:mt-0 md:ml-6">
 
-        <a href="{{ route('cart.clear') }}">Clear Cart</a>
-    @else
-        <p>Your cart is empty.</p>
-    @endif
+                            <h2 class="text-lg font-bold">{{ $cartItem->product->name }}</h2>
+                            <p class="mt-2 text-gray-600">{{ $cartItem->product->description }}</p>
+                            <div class="mt-4 flex items-center">
+                                <span class="mr-2 text-gray-600">Quantity:</span>
+                                <div class="flex items-center">
+                                    <form class="change-quantity-form">
+                                        @csrf
+                                        <input type="hidden" name="product_id" value="{{ $cartItem->product->id }}">
+                                        <button type="button" class="change-quantity-btn bg-gray-200 rounded-l-lg px-2 py-1" data-action="decrement">-</button>
+                                        <span class="quantity mr-2 text-gray-600">{{ $cartItem->quantity }}</span>
+                                        <small class="error"></small>
+                                        <button type="button" class="change-quantity-btn bg-gray-200 rounded-l-lg px-2 py-1" data-action="increment">+</button>
+                                        {{--<input type="number" name="quantity" value="{{ $cartItem->quantity }}" min="1" max="10">--}}
+                                        {{--<input type="submit" value="submit">--}}
+                                        <span class="price ml-auto font-bold">${{ $cartItem->product->price }}</span>
+                                    </form>
+                                </div>
+                            </div>
+                            <a class="destroy" href="{{ route('cart.destroy', ['cartItemId' => $cartItem->id]) }}">Remove</a>
+                        </div>
+                    </div>
+                    <a href="{{ route('cart.clear') }}" class="bg-red-700 hover:bg-red-400 font-bold py-3 px-3 rounded relative inset-3">Clear Cart</a>
+                @endforeach
+            @else
+                Your cart is empty
+            @endif
+        </div>
+    </div>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
     <script>
@@ -41,27 +55,36 @@
                 var product_id = form.find('input[name="product_id"]').val();
                 var action = $(this).data('action');
                 var cartCount = $('.cart-count');
+                var price = {{ isset($cartItem) ? $cartItem->product->price : 0 }};
                 $.ajax({
                     type: 'POST',
                     url: '{{ route('cart.changeQuantity') }}',
                     data: {
                         _token: '{{csrf_token()}}',
                         product_id: product_id,
-                        action: action
+                        action: action,
                     },
                     success: function (response) {
-                        console.log(response.cartCount.quantity);
-                        console.log(response.quantity);
+                        console.log(response);
                         let quantityElement = form.find('.quantity');
                         quantityElement.text(response.quantity);
-                        cartCount.text(response.quantity);
+                        $('.price').text("$"+(response.quantity * price).toFixed(2));
+                        if (response.quantity < 1) {
+                            $('.error').text('Quantity of minimum 1 is required');
+                            $("button[data-action=\"decrement\"]").prop('disabled', true);
+                            cartCount.text("");
+                        } else {
+                            $("button[data-action=\"decrement\"]").prop('disabled', false);
+                        }
+                        cartCount = response.quantity;
+                        $('.cart-count').text(cartCount);
+
                     },
                     error: function () {
                         alert('error occurred');
                     }
-                })
+                });
             });
         });
-
     </script>
 @endsection
