a7f5b85573e4771683541aa4b527335b0a3a46dd
JosephDeFacto <josip.mareljic@gmail.com>
2023-09-03 20:44:31 +0200

    Implement basic search functionality

---
+++ app/Http/Controllers/SearchController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Products;
+use Illuminate\Http\Request;
+
+class SearchController extends Controller
+{
+    public function search(Request $request)
+    {
+        $search = $request->input('q');
+
+        $searchProducts = Products::query()
+            ->where('name', 'LIKE', "%{$search}%")
+            ->get();
+
+
+        //return response()->json($searchProducts);
+        return view('products.products', compact('searchProducts'));
+    }
+}
