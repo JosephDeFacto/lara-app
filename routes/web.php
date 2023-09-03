5f42d94c207f1e46059577350a5a0f579f31af11
JosephDeFacto <josip.mareljic@gmail.com>
2023-09-03 20:42:27 +0200

    Added new routes

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,14 @@
 <?php
 
 use App\Http\Controllers\OrderController;
+use App\Http\Controllers\SearchController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegistrationController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\UserController;
-
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,13 +21,15 @@ use App\Http\Controllers\UserController;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 // registration
-Route::post('register', [RegistrationController::class, 'store']);
 Route::get('register', [RegistrationController::class, 'create']);
+Route::post('register', [RegistrationController::class, 'store']);
 
 // login
 Route::get('login', [LoginController::class, 'create']);
@@ -45,10 +48,18 @@ Route::get('viewCart', [CartController::class, 'viewCart'])->name('cart.viewCart
 Route::get('cart/destroy/{cartItemId}', [CartController::class, 'destroy'])->name('cart.destroy');
 Route::get('cart/clear', [CartController::class, 'clear'])->name('cart.clear');
 Route::post('addToCart', [CartController::class, 'addToCart'])->name('cart.add');
+Route::get('cart/count', [CartController::class, 'getCartCount'])->name('cart.getCount');
+
+Route::post('cart/change-quantity', [CartController::class, 'changeQuantity'])->name('cart.changeQuantity');
 
 // account
-Route::get('account', [UserController::class, 'account']);
+Route::get('account', [UserController::class, 'account'])->name('user.account');
+Route::get('account/update-password', [UserController::class, 'create']);
+Route::post('update-password', [UserController::class, 'store']);
 
 // checkout
 Route::get('checkout', [OrderController::class, 'checkout']);
 Route::get('show', [OrderController::class, 'show']);
+
+// search
+Route::get('/search', [SearchController::class, 'search'])->name('search');
