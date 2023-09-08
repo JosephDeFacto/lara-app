3b6a38ca583a4c49e70dbc600897668662c76b7d
JosephDeFacto <josip.mareljic@gmail.com>
2023-09-08 19:14:52 +0200

    Implement 'toFavorites' feature, where user can mark product as 'favorite'.

--- app/Http/Controllers/FavoriteProductsController.php
+++ app/Http/Controllers/FavoriteProductsController.php
@@ -3,18 +3,35 @@
 namespace App\Http\Controllers;
 
 use App\Models\Favorite;
+use App\Models\Products;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\View\FileViewFinder;
 
 class FavoriteProductsController extends Controller
 {
     public function index()
     {
-        $user = Auth::id();
+        $user_id = auth()->id();
 
-        $products = User::where('id', $user)->with('favorites')->first();
+        $favoriteProducts = Favorite::where('user_id', $user_id)->with('products')->get();
 
-        return view('user/favorites', ['products' => $products]);
+        return view('user.favorites', ['favoriteProducts' => $favoriteProducts]);
+
+    }
+
+    public function store(Request $request)
+    {
+
+        $user_id = Auth::id();
+        $product_id = $request->input('product_id');
+
+        Favorite::create([
+            'user_id' => $user_id,
+            'product_id' => $product_id,
+        ]);
+
+        return redirect()->route('home');
     }
 }
