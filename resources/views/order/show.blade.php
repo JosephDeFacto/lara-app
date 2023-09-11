3b8de2ec2ea9083dd4dd9e7710e0e0e64b9a5b8b
JosephDeFacto <josip.mareljic@gmail.com>
2023-09-11 21:59:00 +0200

    Set initial message if totalPrice is less than zero.

--- resources/views/order/show.blade.php
+++ resources/views/order/show.blade.php
@@ -1,12 +1,29 @@
 @extends('welcome')
 @section('content')
-    @if ($orders)
-    @foreach ($orders->orderItems as $orderItem)
-        {{ $orderItem->product->name }}<br>
-        {{ $orderItem->product->description }}<br>
-        {{ $orderItem->product->image }}<br>
-        {{ $orderItem->quantity }}
+
+    @php
+        $totalPrice = 0;
+    @endphp
+
+    @foreach($orders as $order)
+        @foreach($order->orderItems as $orderItem)
+
+            {{ $orderItem->product->name }}
+            {{ $orderItem->product->description }}
+            {{ $orderItem->quantity }}
+            Subtotal: {{ $subtotal = $orderItem->product->price * $orderItem->quantity }}<br>
+            <img src="{{ asset('storage/images/'.$orderItem->product->image) }}" style="height: 250px; width: 250px;">
+
+            @php
+                $totalPrice += $subtotal;
+            @endphp
+        @endforeach
     @endforeach
+
+    @if($totalPrice > 0)
+        Total Price: {{ $totalPrice }}
+    @else
+        {{ "There is no orders, yet" }}
     @endif
 
 @endsection
